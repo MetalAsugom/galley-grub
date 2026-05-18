49c1bcb44fec8794f7a0876b98a0556b35227673
MetalAsugom <xianmorales01@gmail.com>
2026-05-18 11:13:24 +0200

    feat(extras): :sparkles: Add new abstract class Extra

---
+++ src/main/java/edu/teamrocket/extras/Extra.java
@@ -0,0 +1,13 @@
+package edu.teamrocket.extras;
+
+public abstract class Extra {
+
+    final static String CHEESE;
+
+    final static String SAUCE;
+
+    final static String SIZE_LARGE;
+
+    String extraProduct;
+}
+
