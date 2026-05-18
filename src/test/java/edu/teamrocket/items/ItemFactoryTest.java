035c6f615fbdebb9707455dbe5f36f394b3c5583
Xián Morales Fonseca <xianmorales01@gmail.com>
2026-05-18 22:53:23 +0200

    test(items): :white_check_mark: add tests for ItemFactory

---
+++ src/test/java/edu/teamrocket/items/ItemFactoryTest.java
@@ -0,0 +1,83 @@
+package edu.teamrocket.items;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ItemFactoryTest {
+
+    @BeforeEach
+    public void setup_itemFactory() {
+        ItemFactory.clear();
+        assertEquals(0, ItemFactory.size());
+    }
+
+    @Test
+    public void get_absent_item_test() {
+
+        Item kpatty = ItemFactory.getItem("Krabby Patty", 1.25);
+        assertNotNull(kpatty);
+
+        assertEquals(1, ItemFactory.size());
+
+        Item coral = ItemFactory.getItem("Coral Bits", 1.00);
+        assertNotNull(kpatty);
+
+        assertNotSame(kpatty,coral);
+        assertEquals(2, ItemFactory.size()); 
+    }
+
+    @Test
+    public void get_present_item_test() {
+
+        Item item = ItemFactory.getItem("Krabby Patty", 1.25);
+        assertNotNull(item);
+
+        assertEquals(1, ItemFactory.size());
+
+        Item itemClon = ItemFactory.getItem("Krabby Patty", 1.25);
+        assertEquals(1, ItemFactory.size());
+        assertTrue(item == itemClon);
+    } 
+
+    @Test
+    public void get_extra_item_test() {
+
+        Item item = ItemFactory.getItem("Krabby Patty", 1.25, "cheese");
+        assertNotNull(item);
+
+        assertEquals(1, ItemFactory.size());
+
+        item = ItemFactory.getItem("Coral Bits", 1.00, "medium");
+        assertNotNull(item);
+
+        assertEquals(2, ItemFactory.size()); 
+    }
+
+    @Test
+    public void get_present_extra_item_test() {
+
+        Item item = ItemFactory.getItem("Krabby Patty", 1.25, "cheese");
+        assertNotNull(item);
+        assertEquals(1, ItemFactory.size());
+
+        Item itemClon = ItemFactory.getItem("Krabby Patty", 1.25, "cheese");
+        assertEquals(1, ItemFactory.size());
+        assertTrue(item == itemClon);
+    }
+
+    @Test
+    public void get_regular_extra_item_test() {
+
+        Item kpattywc = ItemFactory.getItem("Krabby Patty", 1.25, "cheese");
+        assertNotNull(kpattywc);
+        assertEquals(1, ItemFactory.size());
+
+        Item kpatty = ItemFactory.getItem("Krabby Patty", 1.25);
+        assertEquals(2, ItemFactory.size());
+        assertNotSame(kpattywc,kpatty);
+    }
+}
